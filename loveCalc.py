3979c2b6e8c017d6d3ca003a52d19d334a4b93ce
Evan Waller <evanwaller03@gmail.com>
2024-01-02 06:24:02 -0600

    more love calc

--- loveCalc.py
+++ loveCalc.py
@@ -2,12 +2,15 @@ print("The Love Calc is calculating your score...")
 name1 = str(input("Person 1: "))
 name2 = str(input("Person 2: "))
 combinedNames = name1+name2
+print(combinedNames)
+print("---------------")
 
 digitOne = 0
 digitTwo = 0
 for letter in combinedNames:
     if letter.lower() in ('t', 'r', 'u', 'e'):
         digitOne += 1
+        print(letter)
     if letter.lower() in ('l', 'o', 'v', 'e'):
         digitTwo += 1
 
@@ -15,3 +18,9 @@ score = int(str(digitOne) + str(digitTwo))
 
 if score < 10 or score > 90:
     print(f"Your score is {score}, you go together like coke and mentos")
+
+elif score < 50 and score > 40:
+    print(f"Your score is {score}, you are alright together.")
+
+else:
+    print(f"Your score is {score}.")
