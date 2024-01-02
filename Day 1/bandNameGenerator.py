02d00afc1c6bd7385a9f57c7d4ab7d8f90cec4ca
Evan Waller <evanwaller03@gmail.com>
2024-01-01 20:13:19 -0600

    added to tip calc for fun

--- Day 1/bandNameGenerator.py
+++ Day 1/bandNameGenerator.py
@@ -1,13 +1,3 @@
 city = input("Welcome to the band name generator. Where did you grow up?\nCity Name: ")
 pet_name = input("What was the name of your first pet?\nPet Name: ")
 print("This could be the name of your band: " + city + " " + pet_name)
-print(len(city+pet_name))
-
-a = input("A: ")
-b =input("B: ")
-
-c = a
-a = b
-b = c
-
-print("A: " + a + " B: " + b)
--- Day 2/tipGenerator.py
+++ Day 2/tipGenerator.py
@@ -3,11 +3,11 @@ print("Welcome to Evan's tip calculator.")
 total_bill = float(input("Total bill amount: $"))
 party_size = int(input("How many people will split the bill: "))
 #total_tax = float(input("Total tax amount: $"))
-# i = 0
-# print("Please enter the name of each party member:")
-# while i < party_size:
-#     party_names.append(input("Person " + str(i+1) + ": "))
-#     i += 1
+i = 0
+print("Please enter the name of each party member:")
+while i < party_size:
+    party_names.append(input("Person " + str(i+1) + ": "))
+    i += 1
 tip_percent = float(input("What percetnage tip would you like to give: "))
 per_person = (total_bill+(total_bill*tip_percent))/party_size
 
@@ -15,4 +15,6 @@ per_person = (total_bill+(total_bill*tip_percent))/party_size
 final_amount = "{:.2f}".format(per_person)
 #print(type(tip_percent)) //learn more about types
 print(f"Each person must pay: ${final_amount}")
+for person in party_names:
+    print(f"{person} must pay: {final_amount}")
 
