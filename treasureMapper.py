cd0a0c1e396043e027d75fe051be6e8925ca2422
Evan Waller <evanwaller03@gmail.com>
2024-01-04 23:02:48 -0600

    treasure hunt

---
+++ treasureMapper.py
@@ -0,0 +1,15 @@
+line_one = ['-','-','-']
+line_two = ['-','-','-']
+line_three = ['-','-','-']
+map = [line_one, line_two, line_three]
+
+hiding_spot = input("Hide your treasure!\nSelection: ")
+
+letter = hiding_spot[0].lower()
+cols = ['a', 'b', 'c']
+
+letter_index = cols.index(letter)
+num_index = int(hiding_spot[1])-1
+
+map[num_index][letter_index] = 'x'
+print(f"{line_one}\n{line_two}\n{line_three}\n")
