03f881a08c93af2d8b9ff6ba64a425d1504463af
Evan Waller <evanwaller03@gmail.com>
2024-01-07 16:12:05 -0600

    updates

--- DAY 8/ceasarsCipher.py
+++ DAY 8/ceasarsCipher.py
@@ -1,3 +1,9 @@
+def encode(message, shift_number):
+    return
+
+def decode(message, shift_number):
+    return
+
 print("Welcome to Ceasars Cipher.")
 
 run_again = True
@@ -11,9 +17,11 @@ while run_again:
             input_not_validated = False
             message_to_encode = input("Message to encode: ")
             set_shift_number = int(input("Shift number: "))
+            encoded_message = encode(message_to_encode, set_shift_number)
 
         elif encode_or_decode == 'd':
             input_not_validated = False
             message_to_decode = input("Message to decode: ")
-            shift_number = int(input("Shift number: "))
-    #
+            decode_shift_number = int(input("Shift number: "))
+            decoded_message = decode(message_to_decode, decode_shift_number)
+    #       
