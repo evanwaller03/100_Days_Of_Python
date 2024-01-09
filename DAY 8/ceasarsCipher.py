d9260a75a1ce11cb6e8909be19376cd9f64e9ab6
Evan Waller <evanwaller03@gmail.com>
2024-01-08 22:32:23 -0600

    encoder now works

--- DAY 8/ceasarsCipher.py
+++ DAY 8/ceasarsCipher.py
@@ -1,11 +1,32 @@
 normal_alphabet = 'abcedfghijklmnopqrstuvwxyz'
+chars_to_skip = ['.', ' ', ',', "'", '!', '$']
 
 def encode(message, shift_number):
-    for n in range(0, len(normal_alphabet)):
-        if (n + shift_number) > len(normal_alphabet):
-            new_index = (n + shift_number) - len(normal_alphabet)
+    encoded_message = ''
+    for letter in message.lower():
+        if letter in chars_to_skip:
+            encoded_message = encoded_message + letter
+            continue
+            
+        current_index  = normal_alphabet.index(str(letter))
+        if (current_index + shift_number) >= len(normal_alphabet):
+            new_index = (current_index + shift_number) - len(normal_alphabet)
         else: 
-            new_index = n + shift_number
+            new_index = current_index + shift_number
+        encoded_message = encoded_message + normal_alphabet[new_index]
+    print(encoded_message)
+        
+    return
+
+        
+
+    # for n in range(0, len(normal_alphabet)):
+    #     print(n)
+    #     if (n + shift_number) > len(normal_alphabet):
+    #         new_index = (n + shift_number) - len(normal_alphabet)
+    #     else: 
+    #         new_index = n + shift_number
+    #     print(new_index)
         
     return 
 
