c8d815687e35fb7ad0df3ab0bc012a24766db277
Evan Waller <evanwaller03@gmail.com>
2024-01-09 20:28:54 -0600

    CC works

--- DAY 8/ceasarsCipher.py
+++ DAY 8/ceasarsCipher.py
@@ -18,8 +18,20 @@ def encode(message, shift_number):
     return encoded_message
 
 def decode(message, shift_number):
-
-    return
+    decoded_message = ''
+    for letter in message.lower():
+        if letter in chars_to_skip:
+            decoded_message = decoded_message + letter
+            continue
+            
+        current_index  = normal_alphabet.index(str(letter))
+        if (current_index - shift_number) < 0:
+            new_index = (current_index - shift_number) + len(normal_alphabet)
+        else: 
+            new_index = current_index - shift_number
+        decoded_message = decoded_message + normal_alphabet[new_index]
+    
+    return decoded_message
 
 print("Welcome to Ceasars Cipher.")
 
@@ -27,19 +39,15 @@ run_again = True
 
 while run_again:
     # Take Input
-    input_not_validated = True
-    while input_not_validated:
-        encode_or_decode = input("Would you like to encode(e) or decode(d)?\nSelection: ")
-        if encode_or_decode == 'e':
-            input_not_validated = False
-            message_to_encode = input("Message to encode: ")
-            set_shift_number = int(input("Shift number: "))
-            encoded_message = encode(message_to_encode, set_shift_number)
-            print(encoded_message)
+    encode_or_decode = input("Would you like to encode(e) or decode(d)?\nSelection: ")
+    if encode_or_decode == 'e':
+        message_to_encode = input("Message to encode: ")
+        set_shift_number = int(input("Shift number: "))
+        encoded_message = encode(message_to_encode, set_shift_number)
+        print(encoded_message)
 
-        elif encode_or_decode == 'd':
-            input_not_validated = False
-            message_to_decode = input("Message to decode: ")
-            decode_shift_number = int(input("Shift number: "))
-            decoded_message = decode(message_to_decode, decode_shift_number)
-    #       
+    elif encode_or_decode == 'd':
+        message_to_decode = input("Message to decode: ")
+        decode_shift_number = int(input("Shift number: "))
+        decoded_message = decode(message_to_decode, decode_shift_number)
+        print(decoded_message)
