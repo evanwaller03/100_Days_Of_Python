ebe41bd67cf9ccfa28bffb1afb330d22081ac69c
Evan Waller <evanwaller03@gmail.com>
2024-01-11 23:47:29 -0600

    combined dictionary with functions

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -13,41 +13,36 @@ def multiply(num1, num2):
 def divide(num1, num2):
     return num1 / num2
 
-
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
 
 def get_value(first_num, operator, second_num):
-    if operator == '+':
-        output = add(first_num, second_num)
-    elif operator == '-':
-        output = subtract(first_num, second_num)
-    if operator == '*':
-        output = multiply(first_num, second_num)
-    if operator == "/":
-        output = divide(first_num, second_num)
+    output = operations[operator](first_num, second_num)
     print(f"{str(first_num)} {operator} {str(second_num)} = {str(output)}")
     return output
 
+need_first_input = True
 
-first_input = True
 while True:
     
-    if first_input:
+    if need_first_input:
         first = int(input("Enter first number: "))
-        operator = str(input("+\n-\n*\n/\nChoose an operator: "))
-        second = int(input("Enter second number: "))
-        output = get_value(first, operator, second)
-    else:
-        operator = str(input("+\n-\n*\n/\nChoose an operator: "))
-        second = int(input("Enter second number: "))
-        output = get_value(first, operator, second)
+
+    operator = str(input("+\n-\n*\n/\nChoose an operator: "))
+    second = int(input("Enter second number: "))
+    output = get_value(first, operator, second)
 
     go_again = input(f"Type 'y to continue calculating with {output}, or type 'n' to start a new calc, or 'x' to quit: ").lower()
     if go_again == 'x':
         break
     if go_again == 'y':
-        first_input = False
+        need_first_input = False
         first = output
     else:
-        first_input = True
+        need_first_input = True
 # need to fix the continuing using number function flow
 print("Thanks for using my calculator!")
