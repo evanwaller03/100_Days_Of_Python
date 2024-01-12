cc4277f1a34c80b353260292c0891afb2dadae96
Evan Waller <evanwaller03@gmail.com>
2024-01-11 23:40:52 -0600

    working continuous calculator

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -30,19 +30,23 @@ def get_value(first_num, operator, second_num):
 
 first_input = True
 while True:
-    first = int(input("Enter first number: "))
-    operator = str(input("+\n-\n*\n/\nChoose an operator: "))
-    second = int(input("Enter second number: "))
+    
     if first_input:
+        first = int(input("Enter first number: "))
+        operator = str(input("+\n-\n*\n/\nChoose an operator: "))
+        second = int(input("Enter second number: "))
         output = get_value(first, operator, second)
     else:
-        
+        operator = str(input("+\n-\n*\n/\nChoose an operator: "))
+        second = int(input("Enter second number: "))
         output = get_value(first, operator, second)
+
     go_again = input(f"Type 'y to continue calculating with {output}, or type 'n' to start a new calc, or 'x' to quit: ").lower()
     if go_again == 'x':
         break
     if go_again == 'y':
         first_input = False
+        first = output
     else:
         first_input = True
 # need to fix the continuing using number function flow
