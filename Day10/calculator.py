533b8a1585f1030d682942ae83ddd35be448e6d1
Evan Waller <evanwaller03@gmail.com>
2024-01-11 23:37:37 -0600

    minor updates to break down the methods further

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -1,36 +1,42 @@
 import os
 print("Welcome to the calculator app.")
 
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
 
 
 def get_value(first_num, operator, second_num):
     if operator == '+':
-        output = first_num + second_num
+        output = add(first_num, second_num)
     elif operator == '-':
-        output = first_num - second_num
+        output = subtract(first_num, second_num)
     if operator == '*':
-        output = first_num * second_num
+        output = multiply(first_num, second_num)
     if operator == "/":
-        output = first_num / second_num
+        output = divide(first_num, second_num)
     print(f"{str(first_num)} {operator} {str(second_num)} = {str(output)}")
     return output
 
-def get_inputs(first_input):
-    if first_input:
-        first = int(input("Enter first number: "))
-    operator = str(input("+\n-\n*\n/\nChoose an operator: "))
-    second = int(input("Enter second number: "))
-    if first_input: 
-        return first, operator, second
-    return operator, second
 
 first_input = True
 while True:
+    first = int(input("Enter first number: "))
+    operator = str(input("+\n-\n*\n/\nChoose an operator: "))
+    second = int(input("Enter second number: "))
     if first_input:
-        first, operator, second = get_inputs(first_input)
         output = get_value(first, operator, second)
     else:
-        operator, second = get_inputs(first_input)
+        
         output = get_value(first, operator, second)
     go_again = input(f"Type 'y to continue calculating with {output}, or type 'n' to start a new calc, or 'x' to quit: ").lower()
     if go_again == 'x':
