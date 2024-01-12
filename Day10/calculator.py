7675522a03beb42224625c5f259af57f7b214192
Evan Waller <evanwaller03@gmail.com>
2024-01-12 00:00:34 -0600

    even better... recursion

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -25,24 +25,27 @@ def get_value(first_num, operator, second_num):
     print(f"{str(first_num)} {operator} {str(second_num)} = {str(output)}")
     return output
 
-need_first_input = True
+def calculator():
+    first = int(input("Enter first number: "))
+    for symbol in operations:
+        print(symbol)
 
-while True:
-    
-    if need_first_input:
-        first = int(input("Enter first number: "))
+    continue_operation = True
 
-    operator = str(input("+\n-\n*\n/\nChoose an operator: "))
-    second = int(input("Enter second number: "))
-    output = get_value(first, operator, second)
+    while continue_operation:
+        operator = str(input("Choose an operator: "))
+        second = int(input("Enter second number: "))
+        output = get_value(first, operator, second)
 
-    go_again = input(f"Type 'y to continue calculating with {output}, or type 'n' to start a new calc, or 'x' to quit: ").lower()
-    if go_again == 'x':
-        break
-    if go_again == 'y':
-        need_first_input = False
-        first = output
-    else:
-        need_first_input = True
-# need to fix the continuing using number function flow
-print("Thanks for using my calculator!")
+        go_again = input(f"Type 'y to continue calculating with {output}, or type 'n' to start a new calc, or 'x' to quit: ").lower()
+        if go_again == 'x':
+            print("Thanks for using my calculator!")
+            break
+        if go_again == 'y':
+            first = output
+
+        else:
+            continue_operation = False
+            calculator()
+
+calculator()
