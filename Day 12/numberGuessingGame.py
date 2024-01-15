f94b827d56b624dea67e9208ede047f613c958c3
Evan Waller <evanwaller03@gmail.com>
2024-01-15 17:51:11 -0600

    start of the # guessing game

---
+++ Day 12/numberGuessingGame.py
@@ -0,0 +1,12 @@
+def getAttempts(attmepts):
+    print(f"You have {attmepts} left to guess the right number.")
+
+def run_game():
+    print("Welcome to the Number Guessing Game.")
+    print("I am thinking of a number 1 through 100.")
+    choose_mode = input("Choose a difficulty. Type 'easy' or 'hard': " )
+    starting_attempts = 10
+
+    
+
+run_game()
