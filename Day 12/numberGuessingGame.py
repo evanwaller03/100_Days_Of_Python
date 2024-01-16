cbe26d71596f2bb136317fe108c2b28cddd69f3d
Evan Waller <evanwaller03@gmail.com>
2024-01-15 18:05:35 -0600

    it works!

--- Day 12/numberGuessingGame.py
+++ Day 12/numberGuessingGame.py
@@ -1,12 +1,46 @@
+import random
+
+def randomNumber():
+    targetNumber = random.randint(1, 100)
+    return targetNumber
+
 def getAttempts(attmepts):
     print(f"You have {attmepts} left to guess the right number.")
+
+def compare_guess(guess, target):
+    if guess == target:
+        print("You win!")
+        return True
+    if guess < target:
+        print("Too low.")
+    if guess > target:
+        print("Too high.")
+    return False
 
 def run_game():
     print("Welcome to the Number Guessing Game.")
     print("I am thinking of a number 1 through 100.")
+    target = randomNumber()
     choose_mode = input("Choose a difficulty. Type 'easy' or 'hard': " )
-    starting_attempts = 10
-
+    if choose_mode == 'easy':
+        starting_attempts = 10
+    else:
+        starting_attempts = 5
     
+    while starting_attempts > 0:
+        getAttempts(starting_attempts)
+        guess = int(input("Make a guess: "))
+        check_guess = compare_guess(guess, target)
+        if check_guess == True:
+            play_again = input("Play again? Type 'yes' or 'no': ")
+            if play_again == 'yes':
+                run_game()
+            else:
+                print('Thanks for playing.')
+                break
+        else:
+            starting_attempts -= 1
+        
+
 
 run_game()
