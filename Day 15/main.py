813d5c4295d6658bd9ddbd1a6a0bae898642f4b6
Evan Waller <evanwaller03@gmail.com>
2024-01-15 21:28:18 -0600

    start of coffee machine

--- Day 15/main.py
+++ Day 15/main.py
@@ -3,11 +3,23 @@ from data import MENU, resources
 WATER = resources["water"]
 MILK = resources["milk"]
 COFFEE = resources["coffee"]
+MONEY = 0
 
 def print_report():
-    return 0
+    print(f"Water: {WATER}\nMilk: {MILK}\nCoffee: {COFFEE}\nMoney: {MONEY}")
+
+def check_resources_available(drink, WATER, MILK, COFFEE):
+    WATER -= drink['ingredients']['water']
+    return WATER
 
 def get_order ():
+    global WATER
     user_order = input("What would you like? (espresso/latte/cappuccino): ")
     if user_order == 'report':
         print_report()
+    print(WATER)
+    drink = MENU[user_order]
+    WATER = check_resources_available(drink, WATER, MILK, COFFEE)
+    print(WATER)
+
+get_order()
