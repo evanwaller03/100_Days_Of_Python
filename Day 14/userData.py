c8ca51c5a756ad48a7d57bb7a5487ec80ae2fbd2
Evan Waller <evanwaller03@gmail.com>
2024-01-15 18:51:27 -0600

    data list is updated for the new game

---
+++ Day 14/userData.py
@@ -0,0 +1,62 @@
+user_data = [
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 364,
+        'description': 'Internet Persona',
+        'country': 'United States',
+    },
+    {
+        'name': 'The Rock',
+        'follower_count': 395,
+        'description': 'Movie Star',
+        'country': 'United States',
+    },
+    {
+        'name': 'Leo Messi',
+        'follower_count': 497,
+        'description': 'Soccer Player',
+        'country': 'Argentina',
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 617,
+        'description': 'Soccer Player',
+        'country': 'Portugal',
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 399,
+        'description': 'Internet Persona',
+        'country': 'United States',
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 265,
+        'description': 'Cricket Player',
+        'country': 'India',
+    },
+    {
+        'name': 'Drake',
+        'follower_count': 145,
+        'description': 'Rapper',
+        'country': 'Canada',
+    },
+    {
+        'name': 'Lebron James',
+        'follower_count': 159,
+        'description': 'Basket Player',
+        'country': 'United States',
+    },
+    {
+        'name': 'Neymar Jr.',
+        'follower_count': 218,
+        'description': 'Soccer Player',
+        'country': 'Brazil',
+    },
+    {
+        'name': 'Connor McGregor',
+        'follower_count': 47,
+        'description': 'UCF Fighter',
+        'country': 'Ireland',
+    },
+]
