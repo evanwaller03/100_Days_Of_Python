1d6404669b1c89de777d5e725f486399e971ec55
Evan Waller <evanwaller03@gmail.com>
2024-01-16 22:15:56 -0600

    more coffee machine

--- Day 15/main.py
+++ Day 15/main.py
@@ -8,20 +8,20 @@ MONEY = 0
 def print_report():
     print(f"Water: {WATER}\nMilk: {MILK}\nCoffee: {COFFEE}\nMoney: ${MONEY}")
 
-def check_resources_available(drink):
-    for ingredient in drink:
-        if drink[ingredient] > resources[ingredient]:
+def check_resources_available(drink_ingredients):
+    for ingredient in drink_ingredients:
+        if drink_ingredients[ingredient] > resources[ingredient]:
             print(f"Sorry, there is not enough {ingredient}.")
             return False
     return True
 
-def process_coint():
+def process_coins():
     print("Please insert coins.")
     total = int(input("Number of Quarters: ")) * .25
     total += int(input("Number of Dimes: ")) * .1
     total += int(input("Number of Nickels: ")) * .05
     total += int(input("Number of Pennies: ")) * .01
-    
+    return total    
 
 is_on = True
 
@@ -33,4 +33,13 @@ while is_on:
         is_on = False
     else:
         drink = MENU[user_order]
-        if check_resources_available(drink):
+        print(drink)
+        if check_resources_available(drink['ingredients']):
+            payment = process_coins()
+            if payment < drink['cost']:
+                print(f"Sorry, please enter more change to buy {drink}")
+            else:
+                MONEY += payment
+                for ingredient in drink['ingredients']:
+                    resources[ingredient] -= drink['ingredients'][ingredient]
+                        
