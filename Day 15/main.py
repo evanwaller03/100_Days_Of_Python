241bddc47b4ec450a1b9f0673bc7231f399c9839
Evan Waller <evanwaller03@gmail.com>
2024-01-16 21:46:37 -0600

    updates to coffee machine

--- Day 15/main.py
+++ Day 15/main.py
@@ -12,14 +12,16 @@ def check_resources_available(drink, WATER, MILK, COFFEE):
     WATER -= drink['ingredients']['water']
     return WATER
 
-def get_order ():
-    global WATER
+is_on = True
+
+while is_on:
     user_order = input("What would you like? (espresso/latte/cappuccino): ")
     if user_order == 'report':
         print_report()
-    print(WATER)
-    drink = MENU[user_order]
-    WATER = check_resources_available(drink, WATER, MILK, COFFEE)
-    print(WATER)
-
-get_order()
+    elif user_order == 'off':
+        is_on = False
+    else:
+        print(WATER)
+        drink = MENU[user_order]
+        WATER = check_resources_available(drink, WATER, MILK, COFFEE)
+        print(WATER)
