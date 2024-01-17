c171ab4f9b7ae0196881bb767e74824d94ebb42b
Evan Waller <evanwaller03@gmail.com>
2024-01-17 08:02:19 -0600

    finalized coffee machine

--- Day 15/main.py
+++ Day 15/main.py
@@ -1,12 +1,9 @@
 from data import MENU, resources
 
-WATER = resources["water"]
-MILK = resources["milk"]
-COFFEE = resources["coffee"]
 MONEY = 0
 
 def print_report():
-    print(f"Water: {WATER}\nMilk: {MILK}\nCoffee: {COFFEE}\nMoney: ${MONEY}")
+    print(f"Water: {resources["water"]}\nMilk: {resources["milk"]}\nCoffee: {resources["coffee"]}\nMoney: ${MONEY}")
 
 def check_resources_available(drink_ingredients):
     for ingredient in drink_ingredients:
@@ -23,6 +20,22 @@ def process_coins():
     total += int(input("Number of Pennies: ")) * .01
     return total    
 
+def transaction_complete(money_received, drink_cost):
+    if money_received >= drink_cost:
+        global MONEY
+        MONEY += payment
+        change = round(money_received - drink_cost,2)
+        print(f"Here is your change: ${change}")
+        return True
+    else:
+        print("Sorry, that is not enough money.")
+        return False
+    
+def make_coffee(drink_name, order_ingredients):
+    for ingredient in order_ingredients:
+        resources[ingredient] -= drink['ingredients'][ingredient]
+    print(f'Here is your {drink_name}.')
+
 is_on = True
 
 while is_on:
@@ -36,10 +49,6 @@ while is_on:
         print(drink)
         if check_resources_available(drink['ingredients']):
             payment = process_coins()
-            if payment < drink['cost']:
-                print(f"Sorry, please enter more change to buy {drink}")
-            else:
-                MONEY += payment
-                for ingredient in drink['ingredients']:
-                    resources[ingredient] -= drink['ingredients'][ingredient]
+            if transaction_complete(payment, drink['cost']):
+                make_coffee(user_order, drink['ingredients'])
                         
