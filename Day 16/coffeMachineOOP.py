09076063e0741db8578cbbd600d20d967eb8a71e
Evan Waller <evanwaller03@gmail.com>
2024-01-19 13:49:19 -0600

    learning about objects

--- Day 16/coffeMachineOOP.py
+++ Day 16/coffeMachineOOP.py
@@ -1,1 +1,10 @@
 # Recreate the coffe machine but with Object Oriented Programming
+
+# Attributes: variables associated with the object
+# Methods: functions that the object can call to execute
+# Class: Blue print for objects that you use in your code
+# Classes are named with PascalCasing
+# car == CarBluePrint()
+# object == Class()
+
+
---
+++ Day 16/turtleSandBox.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+# Set up turtle object
+waller = Turtle() # Create an object
+waller.color("coral") # Call a method and set a color
+waller.shape("turtle")
+waller.forward(100)
+
+# Set up graphics
+waller_screen = Screen()
+waller_screen.exitonclick()
+print(waller_screen.canvheight) # Access an attribute of an object
