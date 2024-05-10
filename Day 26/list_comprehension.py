43f424189143dbbe460f075168261b366a53925e
Evan Waller <evanwaller03@gmail.com>
2024-05-10 08:55:10 -0500

    EASYYYYY

--- Day 26/list_comprehension.py
+++ Day 26/list_comprehension.py
@@ -3,3 +3,15 @@ names = ["Evan", "Ian", "Meaghan", "Jerome", "Albert", "Karen", "Francesca", "Da
 long_names = [name.upper() for name in names if len(name) > 4]
 
 print(long_names)
+
+nums = [1,1,2,3,4,5,6,7,8,9]
+
+squared_nums = [n*n for n in nums]
+
+print(squared_nums)
+
+list_of_strings = input().split(',')
+
+convert = [int(n) for n in list_of_strings if (int(n) % 2) == 0]
+
+print(convert)
